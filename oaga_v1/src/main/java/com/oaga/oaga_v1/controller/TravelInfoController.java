df2698453e637e847f52c5a11e356db22743aac7
h2jin <102580742+h2jin@users.noreply.github.com>
2022-07-11 17:14:23 +0900

    병합

--- oaga_v1/src/main/java/com/oaga/oaga_v1/controller/TravelInfoController.java
+++ oaga_v1/src/main/java/com/oaga/oaga_v1/controller/TravelInfoController.java
@@ -15,30 +15,6 @@ import com.oaga.oaga_v1.service.TravelInfoService;
 @Controller
 public class TravelInfoController {
 	
-//			@Autowired
-//			TravelInfoService travelInfoService;
-			// 메인 화면
-			@GetMapping("/travel_info_form")
-			public String index(@PageableDefault (size =4, sort ="id", direction = Direction.DESC)Pageable pageable, Model model) {
-//				Page<Place> imagePage = travelInfoService.getImageList(pageable);
-//				model.addAttribute("imagePageList",imagePage);
-				return "/travelInfo/travel_info_form";
-			}
-			// 랜드마크
-			@GetMapping("/area_randMark")
-			public String randMark() {
-				return "/travelInfo/randmark_form";
-			}
-			// 지역 맛집
-			@GetMapping("/area_detail")
-			public String vestRestaurant() {
-				return "/travelInfo/detail_form";
-			}
-			
-//			@PostMapping("/imageUpload")
-//			public String imageList(RequestFileDto fileDto) {
-//				travelInfoService.getInfoImage(fileDto);
-//				return "upload Test";
-//			}
+
 			
 }
--- oaga_v1/src/main/java/com/oaga/oaga_v1/service/TravelInfoService.java
+++ oaga_v1/src/main/java/com/oaga/oaga_v1/service/TravelInfoService.java
@@ -1,12 +1,6 @@
 package com.oaga.oaga_v1.service;
 
 public class TravelInfoService {
-//	@Autowired
-//	private TravelInfoRepository travelInfoRepository;
-//	
-//	public Page<Place> getImageList(Pageable pageable){
-//		System.out.println("레파지토리에있는 리스트 라이즈 : " + travelInfoRepository.findAll().size());
-//		return travelInfoRepository.findAll(pageable);
-//	}
+
 	
 }
