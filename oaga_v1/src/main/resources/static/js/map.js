e49bbf4247aae654ee75d06eadf29120708b3bed
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2022-08-04 17:27:39 +0900

    지도 마커찍기

--- oaga_v1/src/main/resources/static/js/map.js
+++ oaga_v1/src/main/resources/static/js/map.js
@@ -1,5 +1,5 @@
 let index = {
-	map: function() {
+	mapf: function() {
 		let data = {
 			address: $("#addressInfo").val(),
 		}
@@ -19,12 +19,22 @@ let index = {
 				var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 				var options = { //지도를 생성할 때 필요한 기본 옵션
 					//	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-					center: new kakao.maps.LatLng(data.documents[0].y,data.documents[0].x), //지도의 중심좌표.
+					center: new kakao.maps.LatLng(data.documents[0].y, data.documents[0].x), //지도의 중심좌표.
 
 					level: 3 //지도의 레벨(확대, 축소 정도)
 				};
 
 				var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+				// 마커가 표시될 위치입니다 
+				var markerPosition = new kakao.maps.LatLng(data.documents[0].y, data.documents[0].x);
+
+				// 마커를 생성합니다
+				var marker = new kakao.maps.Marker({
+					position: markerPosition
+				});
+
+				// 마커가 지도 위에 표시되도록 설정합니다
+				marker.setMap(map);
 
 			},
 			error: function(e) { console.log(e); }
@@ -33,7 +43,7 @@ let index = {
 	},
 }
 
-index.map();
+index.mapf();
 
 
 
